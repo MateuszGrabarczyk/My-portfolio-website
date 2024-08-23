efbf666bfedbd1df660b8a66b5e1a982da1642a6
Mateusz Grabarczyk <stepowa28@gmail.com>
2024-08-23 13:31:41 +0200

    🐛Small fix to paragraph on bigger screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
       <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
         Fullstack developer.
       </h3>
-      <p className="text-md py-5 leading-8 text-gray-800 md:text-xl dark:text-gray-200">
+      <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-lg mx-auto lg:max-w-xl xl:max-w-2xl">
         I'm interested in Backend Development with Python, but I also like
         Frontend technologies, especially React! I love to learn new things and
         I'm a big cycling fan. Currently I'm learning German as well!
