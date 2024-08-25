2bfa34142d3ac82d4cea426f73b14798441677cc
Mateusz Grabarczyk <stepowa28@gmail.com>
2024-08-25 18:39:05 +0200

    ♻️Add SocialLinks component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import Experience from "@/components/Experience";
-import SocialLink from "@/components/SocialLink";
 import Image from "next/image";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { useState } from "react";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import SocialLinks from "@/components/SocialLinks";
 
 function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -19,16 +18,7 @@ function Home() {
         <section className="min-h-screen">
           <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
           <Header />
-          <div className="text-5xl flex justify-center gap-14 dark:text-gray-400">
-            <SocialLink
-              href="https://github.com/MateuszGrabarczyk"
-              Icon={AiFillGithub}
-            />
-            <SocialLink
-              href="https://www.linkedin.com/in/mateusz-grabarczyk-361851210/"
-              Icon={AiFillLinkedin}
-            />
-          </div>
+          <SocialLinks />
           <div className="flex justify-center py-10">
             <Image
               src="/me.jpg"
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,31 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import SocialLink from "@/components/SocialLink";
+import { IconType } from "react-icons";
+
+type SocialLinkItem = {
+  href: string;
+  Icon: IconType;
+};
+
+const socialLinks: SocialLinkItem[] = [
+  {
+    href: "https://github.com/MateuszGrabarczyk",
+    Icon: AiFillGithub,
+  },
+  {
+    href: "https://www.linkedin.com/in/mateusz-grabarczyk-361851210/",
+    Icon: AiFillLinkedin,
+  },
+];
+
+const SocialLinks: React.FC = () => {
+  return (
+    <div className="text-5xl flex justify-center gap-14 dark:text-gray-400">
+      {socialLinks.map((link, index) => (
+        <SocialLink key={index} href={link.href} Icon={link.Icon} />
+      ))}
+    </div>
+  );
+};
+
+export default SocialLinks;
