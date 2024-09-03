09eb962251679ac2048cbd848da8696bfd29c707
Mateusz Grabarczyk <stepowa28@gmail.com>
2024-09-03 14:55:36 +0200

    🐛Fix the menu item text gets smaller on close

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -13,7 +13,7 @@ function NavItem({ href, label, onClick, menuOpen }: NavItemProps) {
       <a
         href={href}
         className={`font-medium text-gray-800 dark:text-gray-300 transition-colors duration-300 ease-in-out hover:text-teal-500 dark:hover:text-teal-300 ${
-          menuOpen ? "text-3xl" : "text-base"
+          menuOpen ? "text-3xl md:text-base" : "text-3xl md:text-base"
         }`}
         onClick={onClick}
       >
